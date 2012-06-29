eb67e0fa0c00a2ef407822abb92d95af9475fdca
Edwin DH laptop <edychrist@gmail.com>
2012-06-29 20:52:04 -0300

    Plataform for tests and setup file created.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os.path
+from setuptools import setup, find_packages
+import sys
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name = "projeto",
+    version = "0.1.1",
+    description = "Edge detection over microscopic images",
+    long_description="\n\n".join([
+        open(os.path.join(base_dir, "README"), "r").read(),
+        open(os.path.join(base_dir, "AUTHORS"), "r").read()
+    ]),
+    url = "http://www.vision.ime.usp.br/~edelgado/projeto.php/",
+    author = "Edwin Delgado H",
+    author_email = "edychrist@gmail.com",
+    packages = find_packages(),
+    zip_safe = False,
+    #install_requires = install_requires,
+    test_suite = "tests.get_tests",
+)
---
+++ tests/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import projeto
+
+
+class UtilsTestCase(unittest.TestCase):
+
+    def test_url_join_http(self):
+        self.assertEqual("http://example.com/", "http://example.com/")
+        
