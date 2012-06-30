f8fd747692434345006cefb7dbdb2b70b98d4c20
Edwin DH laptop <edychrist@gmail.com>
2012-06-29 23:53:10 -0300

    test 9 over travis

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ import unittest
 
 def get_tests():
     start_dir = os.path.dirname(__file__)
-    return self.assertEqual("http://example.com/", "http://example.com/") #return unittest.TestLoader().discover(start_dir, pattern="*.py")
+    return unittest.TestLoader().discover(start_dir, pattern="*.py")
